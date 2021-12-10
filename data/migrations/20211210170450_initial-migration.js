346b27087d5434e27db5900e69f2289a1552ac4e
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-10 15:08:53 -0700

    added to database schema

--- data/migrations/20211210170450_initial-migration.js
+++ data/migrations/20211210170450_initial-migration.js
@@ -30,11 +30,15 @@ exports.up = async function(knex) {
 			.notNullable()
 			.references('project_id')
 			.inTable('projects')
+			.onDelete('CASCADE')
+			.onUpdate('	CASCADE')
 		table.integer('resource_id')
 			.unsigned()
 			.notNullable()
 			.references('resource_id')
 			.inTable('resources')
+			.onDelete('CASCADE')
+			.onUpdate('	CASCADE')
 	})
 };
 
