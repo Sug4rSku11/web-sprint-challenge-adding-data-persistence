f21d3f32246fabe7887db85cb3044d9d9f3a92ba
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-10 11:25:40 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 
+const ProjectRouter = require('./project/router')
+//const ResourceRouter = require('./resource/router')
+//const TaskRouter = require('./task/router')
+
+
 const server = express()
 
 server.use(express.json())
-
+server.use('/project/router', ProjectRouter)
+//server.use('/resource/router', ResourceRouter)
+//server.use('/task/router', TaskRouter)
 module.exports = server;
